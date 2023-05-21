9e4a16eac4cf67423dbd660486a6a0aab38e7951
lhhuy <lhh206198@gmail.com>
2023-05-21 10:35:02 +0700

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by LHH on 21/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
